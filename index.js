4d02f40add94691584ec70c6580fab6f5afbe7b0
GMMemon875 <ghulammemon875@gmail.com>
2024-07-26 01:09:42 +0500

    add a nots section file

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const fs = require("fs");
-const { title } = require("process");
+const express = require("express"); // sab se pahly ham express ko requre kar ke ek server create karengi
+const app = express(); //express ko app name ke varibale men save karengi
+const path = require("path"); // kese be path ko join karni ke lei path ko require keya jata he
 
-app.set("views enjine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+const fs = require("fs");
+// const { title } = require("process");
+
+app.set("views enjine", "ejs"); // ejs ko install kar ke us ko setup karen gi (app) men
+app.use(express.json()); // / express.json middleware ko use karte hain taake incoming request body ko JSON format mein parse kiya ja sake
+app.use(express.urlencoded({ extended: true })); // express.urlencoded middleware ko use karte hain taake incoming request body ko URL-encoded format mein parse kiya ja sake. 'extended: true' option ka matlab hai ki hum query string parsing mein qs library ka use kar rahe hain jo complex objects ko handle kar sakta hai.
+app.use(express.static(path.join(__dirname, "public"))); // static ka matlb hota he ke koie be Html files ko add karni lei html csss javascript ye sab static file hen  public us folder ka name he jis men ham ni ye sab static file rakhe hen __dirname matlb us men file ko dhondo
 
 app.get("/", function (req, res) {
-  fs.readdir(`./file`, function (err, file) {
+  fs.readdir(`./files`, function (err, file) {
     // console.log(file);
     res.render("index.ejs", { file: file });
   });
 });
 
 app.post("/create", function (req, res) {
-  fs.writeFile(`./file/${req.body.title.txt}`, req.body.detail, function (err) {
-    if (err) {
-      console.log("error");
-    } else {
+  fs.writeFile(
+    `./files/${req.body.name.split(" ").join("")}.txt`,
+    req.body.content,
+    "utf8",
+    function (err) {
       res.redirect("/");
     }
-  });
+  );
 });
- 
+
 app.listen(3000, function () {
   console.log("3000 port is ready");
 });
