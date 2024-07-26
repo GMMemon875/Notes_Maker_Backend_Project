57269bf125b916ff14a79173dc55dfb8127a10e9
GMMemon875 <ghulammemon875@gmail.com>
2024-07-27 00:50:51 +0500

    add a read more section

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.post("/create", function (req, res) {
   );
 });
 
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show.ejs", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
 app.listen(3000, function () {
   console.log("3000 port is ready");
 });
