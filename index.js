07a072bb3ad7426e59f6ba2d899e0fe35d003e77
GMMemon875 <ghulammemon875@gmail.com>
2024-07-30 00:36:49 +0500

    add sam comments

--- index.js
+++ index.js
@@ -11,28 +11,36 @@ app.use(express.urlencoded({ extended: true })); // express.urlencoded middlewar
 app.use(express.static(path.join(__dirname, "public"))); // static ka matlb hota he ke koie be Html files ko add karni lei html csss javascript ye sab static file hen  public us folder ka name he jis men ham ni ye sab static file rakhe hen __dirname matlb us men file ko dhondo
 
 app.get("/", function (req, res) {
+  // app ko / route per call karengi our .files ko fs readfile ke help se files ko read karengi
   fs.readdir(`./files`, function (err, file) {
+    // fs readfile men function jis men err and file argument pass keya he
     // console.log(file);
-    res.render("index.ejs", { file: file });
+    res.render("index.ejs", { file: file }); // fer jab fs.readfile ke help se file read ho ke index .ejs per render karoo and object file ko pass karoo
   });
 });
 
 app.post("/create", function (req, res) {
+  // post / create men fs. write file ke help se ek new file creat karengi
   fs.writeFile(
-    `./files/${req.body.name.split(" ").join("")}.txt`,
-    req.body.content,
-    "utf8",
+    `./files/${req.body.name.split(" ").join("")}.txt`, // .files ke location per file create hp our jo kuch waha input men inter ho text us ko req.body.name save he us ko split karoo ek alag alag array men fer us sab array ko .join ke help se join karoo to space katam ho jaien ge
+    req.body.content, // same prosess content men be ho
+    "utf8", // utf8 ham jab waha se data aie ga to wo buffur form men data hoga to text form men convert karni ke lei use hota he
     function (err) {
+      // fer jab ye sab kam ho jaien to reirect karoo / route pe
       res.redirect("/");
     }
   );
 });
 
+// ab ham ni nots to lekh leya us nots ko hm kese ek new page per dekh sakte hen
+
 app.get("/file/:filename", function (req, res) {
+  // us ko ham / file route per dekh sakte hen
   fs.readFile(
-    `./files/${req.params.filename}`,
-    "utf-8",
+    `./files/${req.params.filename}`, // same wohe tareka jese uper karte aa rahen hen text ko show karni ke lei
+    "utf-8", // text buffur formate men a rahe he to text men convert krani ke lei
     function (err, filedata) {
+      // wo jo data save ho aa he us ko hamm filedata men save keya fer us ko show.ejs file ke help se display keya
       res.render("show.ejs", {
         filename: req.params.filename,
         filedata: filedata,
@@ -41,5 +49,6 @@ app.get("/file/:filename", function (req, res) {
   );
 });
 app.listen(3000, function () {
+  // hamara app yani sarver 3000 port per chal raha he listen kar rahaa he
   console.log("3000 port is ready");
 });
